11a09dcffec5ad60f7419cc3415fed292b350257
Zach Liebeskind <zliebeskind@gmail.com>
2023-07-06 10:09:38 -0500

    First_Commit

---
+++ Week 1/1/main.py
@@ -0,0 +1,25 @@
+# Example code
+
+print("Hello World!")
+
+# My Prediction - Add your prediction as a comment below.
+ 
+ #It will output the text: Hello World!
+
+
+##### Task Investigate
+
+# Which part of the example code is the output statement?
+ # Answer: Hello World!
+
+# Which part of the example code is the string?
+ # Answer: "Hello World!"
+
+# What would the output of the code print("I love Computing") be?
+ # Answer: I love Computing
+
+# What would happen if the code print("I love Comping") was run?
+ # Answer: It would output the text: I love Comping
+
+# What would happen if the code print("I love Computing" was run?
+ # Answer: There would be a syntax error, because there is no ending bracket.
